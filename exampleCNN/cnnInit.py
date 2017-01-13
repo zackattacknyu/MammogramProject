0541cacc0a2f09f23970ec34efcc15aa413de301
zackattacknyu <zackattacknyu@gmail.com>
2017-01-12 17:17:10 -0800

    df

--- exampleCNN/cnnInit.py
+++ exampleCNN/cnnInit.py
@@ -2,23 +2,27 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-from urllib import urlretrieve
-import cPickle as pickle
+
+import pickle
 import os
 import gzip
 
 import numpy as np
-import theano
-
-import lasange
-from lasange import layers
-from lasagne.update import nesterov_momentum
-
-from nolearn.lasange import NeuralNet
-from nolearn.lasange import visualize
+# import theano
+#
+# import lasange
+# from lasange import layers
+# from lasagne.update import nesterov_momentum
+#
+# from nolearn.lasange import NeuralNet
+# from nolearn.lasange import visualize
 
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
 
 
+
+filename = 'mnist.pkl.gz'
+with gzip.open(filename, 'rb') as f:
+    data = pickle.load(f)
